463118490e0049aa294c1388b9dbea4fcbd3ccd8
Asintota <81699016+Asintota@users.noreply.github.com>
2025-02-04 18:12:40 +0100

    hj

--- database.py
+++ database.py
@@ -2,12 +2,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+import os
 # Declare the Base
 Base = declarative_base()
 
 # Database URL
-SQLALCHEMY_DATABASE_URL = "MYSQLDATABASE_URL"  # Replace with your actual DB URL
+SQLALCHEMY_DATABASE_URL = os.getenv("MYSQLDATABASE_URL")  # Replace with your actual DB URL
 
 # Create the Engine and SessionLocal
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
